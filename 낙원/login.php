cfe76f7792281bcd392f516daf7957c0c1a3f130
hwanghj09 <101453046+hwanghj09@users.noreply.github.com>
2023-12-30 16:58:44 +0900

    Add files via upload

---
+++ 낙원/login.php
@@ -0,0 +1,79 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $servername = "svc.sel4.cloudtype.app:31934"; // replace with your actual database host
+    $username = "root";
+    $password = "nagwon";
+    $dbname = "your_database_name"; // replace with your actual database name
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Get data from the form
+    $inputUsername = $_POST['username'];
+    $inputPassword = $_POST['password'];
+
+    // Check if the username exists
+    $checkQuery = "SELECT * FROM users WHERE username='$inputUsername'";
+    $checkResult = $conn->query($checkQuery);
+
+    if ($checkResult->num_rows > 0) {
+        // Verify the password
+        $row = $checkResult->fetch_assoc();
+        $hashedPassword = $row['password'];
+
+        if (password_verify($inputPassword, $hashedPassword)) {
+            // Password is correct, start a session
+            session_start();
+
+            // Store data in session variables
+            $_SESSION['username'] = $inputUsername;
+
+            // Redirect to the main page or any other secured page
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Error: Incorrect password.";
+        }
+    } else {
+        echo "Error: User not found.";
+    }
+
+    // Close the database connection
+    $conn->close();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Planet: Mango - 로그인</title>
+    <!-- Add your CSS styles or link to external stylesheet here -->
+</head>
+
+<body>
+    <h2>로그인</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <label for="username">아이디:</label>
+        <input type="text" id="username" name="username" required>
+
+        <label for="password">비밀번호:</label>
+        <input type="password" id="password" name="password" required>
+
+        <button type="submit">로그인</button>
+    </form>
+</body>
+
+</html>
---
+++ 낙원/signup.php
@@ -0,0 +1,76 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $servername = "svc.sel4.cloudtype.app:31934"; // replace with your actual database host
+    $username = "root";
+    $password = "nagwon";
+    $dbname = "users"; // replace with your actual database name
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Get data from the form
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+
+    // Check if the username is already taken
+    $checkQuery = "SELECT * FROM users WHERE username='$username'";
+    $checkResult = $conn->query($checkQuery);
+
+    if ($checkResult->num_rows > 0) {
+        echo "Error: Username already taken.";
+    } else {
+        // Insert user into the database
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $insertQuery = "INSERT INTO users (username, password, email) VALUES ('$username', '$hashedPassword', '$email')";
+
+        if ($conn->query($insertQuery) === TRUE) {
+            echo "<script>window.location.href = 'index.php';</script>";
+            exit();
+        } else {
+            echo "Error: " . $insertQuery . "<br>" . $conn->error;
+        }
+    }
+
+    // Close the database connection
+    $conn->close();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Planet: Mango - 회원가입</title>
+    <!-- Add your CSS styles or link to external stylesheet here -->
+</head>
+
+<body>
+    <h2>회원가입</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <label for="username">아이디:</label>
+        <input type="text" id="username" name="username" required>
+
+        <label for="password">비밀번호:</label>
+        <input type="password" id="password" name="password" required>
+
+        <label for="email">이메일:</label>
+        <input type="email" id="email" name="email" required>
+
+        <button type="submit">가입하기</button>
+    </form>
+</body>
+
+</html>
