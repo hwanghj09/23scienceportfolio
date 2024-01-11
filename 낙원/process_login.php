b8ce158d29ee3db7e3dc8323d939f6a3722ce63e
hwanghj09 <101453046+hwanghj09@users.noreply.github.com>
2024-01-11 22:09:04 +0900

    Add files via upload

--- 낙원/process_login.php
+++ 낙원/process_login.php
@@ -1,23 +1,28 @@
 <?php
-error_reporting( E_ALL );
-ini_set( "display_errors", 1 );
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 $servername = "svc.sel4.cloudtype.app:32632";
-$username = "root";
+$username1 = "root";
 $password = "qwaszx77^^";
 $dbname = "ip";
 
 // 사용자가 입력한 데이터 받아오기
-if(isset($_POST['login']) && isset($_POST['password'])){
-    $username = $_POST['login'];    
+if (isset($_POST['login']) && isset($_POST['password'])) {
+    $username = $_POST['login'];
     $password = $_POST['password'];
 
     // 데이터베이스 연결
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username1, $password, $dbname);
 
     // 연결 확인
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+
+    // SQL 주입 방지를 위해 사용자 입력 값 이스케이프
+    $username = $conn->real_escape_string($username);
+    $password = $conn->real_escape_string($password);
 
     // 사용자 정보 데이터베이스에 저장 (비밀번호는 암호화하지 않고 저장)
     $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
